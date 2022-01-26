6833685b65d4000cd0e1dacfc59b3f682ad43217
Michal <michalsidzej@gmail.com>
2022-01-26 13:07:11 +0100

    removed development changes

--- src/provider.ts
+++ src/provider.ts
@@ -35,23 +35,19 @@ export class WrappedPolyjuiceProvider extends EventEmitter {
       return [this.polyjuiceWallet.address];
     }
 
-    try {
-      if (args.method === "eth_sendTransaction") {
-        const params = args.params as any[];
-        const newParams = { ...omit(params[0], "gas") };
+    if (args.method === "eth_sendTransaction") {
+      const params = args.params as any[];
+      const newParams = { ...omit(params[0], "gas") };
 
-        const result = await this.polyjuiceWallet.sendTransaction(newParams);
+      const result = await this.polyjuiceWallet.sendTransaction(newParams);
 
-        return result.hash;
-      }
-
-      const result = await this.polyjuiceProvider.send(
-        args.method,
-        args.params as any[]
-      );
-      return result;
-    } catch (e) {
-      console.trace(e);
+      return result.hash;
     }
+
+    const result = await this.polyjuiceProvider.send(
+      args.method,
+      args.params as any[]
+    );
+    return result;
   }
 }
